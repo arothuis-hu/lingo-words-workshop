4c310d0c53e27b0948ac5b2d0de0383ea0dd3ea6
Robin Kuijt <58628107+rkuijt@users.noreply.github.com>
2020-12-03 10:32:44 +0100

    Added LingoWordFilterTest with TDD (green)

--- src/main/java/nl/hu/vkbep/example/lingo/words/core/domain/LingoWordFilter.java
+++ src/main/java/nl/hu/vkbep/example/lingo/words/core/domain/LingoWordFilter.java
@@ -3,6 +3,6 @@ package nl.hu.vkbep.example.lingo.words.core.domain;
 public class LingoWordFilter implements WordFilter {
 	@Override
 	public boolean verify(String word) {
-		return false;
+		return true;
 	}
 }
--- src/test/java/nl/hu/vkbep/example/lingo/words/core/domain/LingoWordFilterTest.java
+++ src/test/java/nl/hu/vkbep/example/lingo/words/core/domain/LingoWordFilterTest.java
@@ -1,7 +1,20 @@
 package nl.hu.vkbep.example.lingo.words.core.domain;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class LingoWordFilterTest {
 
+
+
+	@Test
+	void accepts_words_of_5_letters() {
+		LingoWordFilter filter = new LingoWordFilter();
+		String word = "pizza";
+
+		boolean accepts = filter.verify(word);
+
+		assertTrue(accepts);
+	}
 }
