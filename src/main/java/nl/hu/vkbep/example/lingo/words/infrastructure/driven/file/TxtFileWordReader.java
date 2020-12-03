bff7f7bbcd06b8a661ac05e8db2f64649dc37e8f
Robin Kuijt <58628107+rkuijt@users.noreply.github.com>
2020-12-03 11:39:59 +0100

    Scaffold TxtFileWordReader and Writer

---
+++ src/main/java/nl/hu/vkbep/example/lingo/words/infrastructure/driven/file/TxtFileWordReader.java
@@ -0,0 +1,6 @@
+package nl.hu.vkbep.example.lingo.words.infrastructure.driven.file;
+
+import nl.hu.vkbep.example.lingo.words.core.ports.WordReader;
+
+public class TxtFileWordReader implements WordReader {
+}
---
+++ src/main/java/nl/hu/vkbep/example/lingo/words/infrastructure/driven/file/TxtFileWordWriter.java
@@ -0,0 +1,6 @@
+package nl.hu.vkbep.example.lingo.words.infrastructure.driven.file;
+
+import nl.hu.vkbep.example.lingo.words.core.ports.WordWriter;
+
+public class TxtFileWordWriter implements WordWriter {
+}
