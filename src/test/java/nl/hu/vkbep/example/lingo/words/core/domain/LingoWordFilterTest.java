9bd3805fb2386c4824d0f69076f19254de631761
Robin Kuijt <58628107+rkuijt@users.noreply.github.com>
2020-12-03 10:47:01 +0100

    Extract test setup to beforeEach

--- src/test/java/nl/hu/vkbep/example/lingo/words/core/domain/LingoWordFilterTest.java
+++ src/test/java/nl/hu/vkbep/example/lingo/words/core/domain/LingoWordFilterTest.java
@@ -1,14 +1,21 @@
 package nl.hu.vkbep.example.lingo.words.core.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class LingoWordFilterTest {
 
+	private LingoWordFilter filter;
+
+	@BeforeEach
+	void beforeEach() {
+		filter = new LingoWordFilter();
+	}
+
 	@Test
 	void accepts_words_of_5_letters() {
-		LingoWordFilter filter = new LingoWordFilter();
 		String word = "pizza";
 
 		boolean accepts = filter.verify(word);
@@ -18,7 +25,6 @@ class LingoWordFilterTest {
 
 	@Test
 	void accepts_words_of_6_letters() {
-		LingoWordFilter filter = new LingoWordFilter();
 		String word = "pizzas";
 
 		boolean accepts = filter.verify(word);
@@ -28,7 +34,6 @@ class LingoWordFilterTest {
 
 	@Test
 	void rejects_words_of_4_letters() {
-		LingoWordFilter filter = new LingoWordFilter();
 		String word = "bier";
 
 		boolean accepts = filter.verify(word);
