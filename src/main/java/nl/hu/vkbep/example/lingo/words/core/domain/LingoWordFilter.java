c4b4fa64a9c8f89378c6a03c39855aee3eec748e
Robin Kuijt <58628107+rkuijt@users.noreply.github.com>
2020-12-03 11:32:06 +0100

    Finish LingoWordFilter

--- src/main/java/nl/hu/vkbep/example/lingo/words/core/domain/LingoWordFilter.java
+++ src/main/java/nl/hu/vkbep/example/lingo/words/core/domain/LingoWordFilter.java
@@ -3,6 +3,6 @@ package nl.hu.vkbep.example.lingo.words.core.domain;
 public class LingoWordFilter implements WordFilter {
 	@Override
 	public boolean verify(String word) {
-		return word.matches("[a-z]{5,7}");
+		return word.matches("^[a-z]{5,7}$");
 	}
 }
--- src/test/java/nl/hu/vkbep/example/lingo/words/core/domain/LingoWordFilterTest.java
+++ src/test/java/nl/hu/vkbep/example/lingo/words/core/domain/LingoWordFilterTest.java
@@ -1,7 +1,6 @@
 package nl.hu.vkbep.example.lingo.words.core.domain;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -11,12 +10,25 @@ import java.util.stream.Stream;
 import static org.junit.jupiter.api.Assertions.*;
 
 class LingoWordFilterTest {
-
 	private LingoWordFilter filter;
 
 	@BeforeEach
 	void beforeEach() {
 		filter = new LingoWordFilter();
+	}
+
+	@ParameterizedTest
+	@MethodSource("provideWordsOfVaryingLength")
+	void accepts_words_of_5_6_7_letters_only(String word, boolean shouldAccept) {
+		boolean accepts = filter.verify(word);
+		assertEquals(shouldAccept, accepts);
+	}
+
+	@ParameterizedTest
+	@MethodSource("provideWordsOfVaryingCaseSymbols")
+	void accepts_words_of_lowercase_letters_only(String word, boolean shouldAccept) {
+		boolean accepts = filter.verify(word);
+		assertEquals(shouldAccept, accepts);
 	}
 
 	static Stream<Arguments> provideWordsOfVaryingLength() {
@@ -30,28 +42,19 @@ class LingoWordFilterTest {
 		);
 	}
 
-	@ParameterizedTest
-	@MethodSource("provideWordsOfVaryingLength")
-	void accepts_words_of_5_6_7_letters(String word, boolean shouldAccept) {
-		boolean accepts = filter.verify(word);
-		assertEquals(shouldAccept, accepts);
-	}
-
-	static Stream<Arguments> provideWordsOfVaryingCase() {
+	static Stream<Arguments> provideWordsOfVaryingCaseSymbols() {
 		return Stream.of(
 				Arguments.of("pizza", true),
 				Arguments.of("Pizza", false),
 				Arguments.of("PiZZa", false),
 				Arguments.of("PIZZA", false),
 				Arguments.of("pizzA", false),
-				Arguments.of("piZza", false)
+				Arguments.of("piZza", false),
+				Arguments.of("piZzä", false),
+				Arguments.of("p!zza", false),
+				Arguments.of("P!ZZA", false),
+				Arguments.of("piZ a", false),
+				Arguments.of("p̣̝̔̂͝i̻͚ͅz̰̟̖̩̩̘z̖̭̞͉͍̕a͖͛̓͘", false)
 		);
-	}
-
-	@ParameterizedTest
-	@MethodSource("provideWordsOfVaryingCase")
-	void accepts_words_of_lowercase_only(String word, boolean shouldAccept) {
-		boolean accepts = filter.verify(word);
-		assertEquals(shouldAccept, accepts);
 	}
 }
