a8df844a896532a26320724fc85c04c5982c7035
Robin Kuijt <58628107+rkuijt@users.noreply.github.com>
2020-12-03 11:17:23 +0100

    LingoWordFilter added test for lowercase & refactor simplified code

--- src/main/java/nl/hu/vkbep/example/lingo/words/core/domain/LingoWordFilter.java
+++ src/main/java/nl/hu/vkbep/example/lingo/words/core/domain/LingoWordFilter.java
@@ -3,6 +3,6 @@ package nl.hu.vkbep.example.lingo.words.core.domain;
 public class LingoWordFilter implements WordFilter {
 	@Override
 	public boolean verify(String word) {
-		return word.length() >= 5;
+		return word.matches("[a-z]{5,7}");
 	}
 }
--- src/test/java/nl/hu/vkbep/example/lingo/words/core/domain/LingoWordFilterTest.java
+++ src/test/java/nl/hu/vkbep/example/lingo/words/core/domain/LingoWordFilterTest.java
@@ -24,13 +24,33 @@ class LingoWordFilterTest {
 				Arguments.of("pizza", true),
 				Arguments.of("pizzas", true),
 				Arguments.of("backend", true),
-				Arguments.of("beer", false)
+				Arguments.of("beer", false),
+				Arguments.of("software", false),
+				Arguments.of("developer", false)
 		);
 	}
 
 	@ParameterizedTest
 	@MethodSource("provideWordsOfVaryingLength")
 	void accepts_words_of_5_6_7_letters(String word, boolean shouldAccept) {
+		boolean accepts = filter.verify(word);
+		assertEquals(shouldAccept, accepts);
+	}
+
+	static Stream<Arguments> provideWordsOfVaryingCase() {
+		return Stream.of(
+				Arguments.of("pizza", true),
+				Arguments.of("Pizza", false),
+				Arguments.of("PiZZa", false),
+				Arguments.of("PIZZA", false),
+				Arguments.of("pizzA", false),
+				Arguments.of("piZza", false)
+		);
+	}
+
+	@ParameterizedTest
+	@MethodSource("provideWordsOfVaryingCase")
+	void accepts_words_of_lowercase_only(String word, boolean shouldAccept) {
 		boolean accepts = filter.verify(word);
 		assertEquals(shouldAccept, accepts);
 	}
